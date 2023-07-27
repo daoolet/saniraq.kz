e781988c51d53cb428a950d142655f2f272f949c
daoolet <dauletkhabidullin@gmail.com>
2023-07-27 12:49:50 +0500

    second migration, ads table

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from saniraq.database import Base
-from saniraq.models import User
+from saniraq.models import User, Ad
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
